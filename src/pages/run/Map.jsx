6ee2fadbe7f8803d3662a69ee1e055966075a8a8
ldelbel <lucdelbel@gmail.com>
2021-03-12 09:51:28 -0300

    Change geolocation methods and add distance calculation

--- src/pages/run/Map.jsx
+++ src/pages/run/Map.jsx
@@ -2,22 +2,10 @@
 
 import React, { useState, useEffect } from 'react'
 import { Map, GoogleApiWrapper } from 'google-maps-react';
-import { useGeolocation } from 'react-use';
 
-const MapContainer = () => {
-  const geo = useGeolocation();
-  const [location, setLocation] = useState({ lat: 0, lng: 0 });
 
-  useEffect(()=> {
-    console.log(geo)
-    if(geo.loading === false) {
-      setTimeout(() => {
-        setLocation({ lat: geo.latitude, lng: geo.longitude } )
-        console.log(location)
-      }, 15000)
-    }
-
-  }, [geo.loading, location])
+const MapContainer = props => {
+  
 
   const mapStyles = {
     width: '100%',
@@ -30,7 +18,7 @@ const MapContainer = () => {
       google={google}
       zoom={17}
       style={mapStyles}
-      center={{ lat: location.lat, lng: location.lng }}
+      center={{ lat: props.location.lat, lng: props.location.lng }}
     />
   )
 }
--- src/pages/run/index.jsx
+++ src/pages/run/index.jsx
@@ -1,16 +1,52 @@
-import React, { useState } from 'react';
+/*global google*/
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import GoogleApiWrapper from './Map';
+import { useGeolocation } from 'react-use';
 
 const Run = props => {
-  
-  
+  var location = useGeolocation();
+  const [distance, setDistance] = useState(0);
+  var breadcrumbs = [];
+  console.log(distance)
+
+  const haversine_distance = (mk1, mk2) => {
+    var R = 6371.0710; // Radius of the Earth in kilometers
+    var rlat1 = mk1.position.lat * (Math.PI/180); // Convert degrees to radians
+    var rlat2 = mk2.position.lat * (Math.PI/180); // Convert degrees to radians
+    var difflat = rlat2-rlat1; // Radian difference (latitudes)
+    var difflon = (mk2.position.lng-mk1.position.lng * (Math.PI/180)); // Radian difference (longitudes)
+    var d = 2 * R * Math.asin(Math.sqrt(Math.sin(difflat/2)*Math.sin(difflat/2)+Math.cos(rlat1)*Math.cos(rlat2)*Math.sin(difflon/2)*Math.sin(difflon/2)));
+    return d;
+  }
+
+  const addBreadcrumb = () => {
+    const { latitude, longitude, timestamp } = location;
+    var breadcrumb = { position: { lat: latitude, lng: longitude }, timestamp: timestamp };
+    breadcrumbs.push(breadcrumb);
+    updateDistance();
+  }
+
+  const updateDistance = () => {
+    if(breadcrumbs.length >= 2) {
+      var newDist = distance + haversine_distance(breadcrumbs[breadcrumbs.length-1], breadcrumbs[breadcrumbs.length-2])
+      setDistance(newDist);
+    }
+  }
+
+  useEffect(() => {
+    if(!location.loading){
+      addBreadcrumb();
+      var running = setInterval(addBreadcrumb, 15000)
+      // clearInterval(running)
+    }
+  },[location.loading])
+
   return (
     <>
       <div>
-        {/* {JSON.stringify(state, null, 2)} */}
-      </div>
-      <GoogleApiWrapper />
+        {distance}
+      </div>      
     </>
   );
 }
