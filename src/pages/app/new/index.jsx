ce4bc0281a17b00dfcae54d2a7c3852451b91c34
ldelbel <lucdelbel@gmail.com>
2021-03-17 10:56:53 -0300

    Set router for new run

--- src/pages/app/new/index.jsx
+++ src/pages/app/new/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import styles from './styles/New.module.scss';
 import img from '../../../common/assets/running.png';
 
 const New = props => {
+  let history = useHistory();
+
   return (
     <main className={styles.bg}>
       <div className={styles.container}>
         <img className={styles.img} src={img} alt="run"/>
-        <button className={styles.button}>
+        <button
+        className={styles.button}
+        onClick={() => history.push('/run')}
+        >
           New Running Session
         </button>
       </div>
