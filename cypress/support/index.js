c2d1e043c17680fc3234d2f8d310921a66f5d5cf
Lucas Delbel <lucdelbel@gmail.com>
2021-03-26 00:14:26 -0300

    Cypress video update

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,8 +19,4 @@ import '@testing-library/cypress/add-commands';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-  })
+Cypress.on('uncaught:exception', () => false);
